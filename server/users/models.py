634a0f67bba578b49f7247d96a9dea06d4f41931
MKowalski8 <makow.088@gmail.com>
2024-02-04 06:35:02 +0100

    user_server extend

--- server/users/models.py
+++ server/users/models.py
@@ -12,5 +12,6 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     role = db.Column(db.String(10), nullable=False, default="user") 
+    birthday = db.Column(db.DateTime, nullable=False)
     avatar = db.Column(db.String(200), nullable=True)
     about = db.Column(db.Text, nullable=True)
--- server/users/utils.py
+++ server/users/utils.py
@@ -2,15 +2,46 @@
 from argon2 import PasswordHasher
 import jwt
 import datetime
+import random
+import string
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 
 ph = PasswordHasher()
 SECRET_KEY = 'your_secret_key_here'
 
 def generate_token(user_id, role):
-    # To jeszcze do przeanalizowania
     exp_time = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     token = jwt.encode({'user_id': user_id, 'role': role, 'exp': exp_time}, SECRET_KEY)
-    return token.decode('UTF-8')
+
+    return token
 
 def verify_password(hashed_password, password):
     return ph.verify(hashed_password, password)
+
+def generate_random_password(length=8):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+def send_email(receiver_email, message):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = 'wdaisupersite@gmail.com'
+        msg['To'] = receiver_email
+        msg['Subject'] = 'Password Reset'
+
+        msg.attach(MIMEText(message, 'plain'))
+
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()  
+        server.login('wdaisupersite@gmail.com', 'klei teqb xgqw rwyz') 
+        text = msg.as_string()
+        server.sendmail('wdaisupersite@gmail.com', receiver_email, text)  
+        server.quit()  
+        print('done')
+        return True, None  
+    except Exception as e:
+        print(e)
+        return False, str(e)  
